bec3e13e558f177d623264b104bea1cb3137e81b
Secure-OS <189855621+390sec@users.noreply.github.com>
2026-03-14 21:38:57 +0530

    Create  Thinking, Logic & Problem Solving.m

---
+++ Thinking, Logic & Problem Solving.m
@@ -0,0 +1,29 @@
+
+
+1. Solving Problems with Creative and Critical Thinking
+Provider: IBM
+Course Link: https://www.coursera.org/learn/solving-problems-with-creative-and-critical-thinking
+
+2. Critical Thinking for the Information Age
+Provider: University of Michigan
+Course Link: https://www.coursera.org/learn/critical-thinking-information-age
+
+3. Creative Problem Solving
+Provider: University of Minnesota
+Course Link: https://www.coursera.org/learn/creative-problem-solving
+
+4. Decision‑Making and Scenarios
+Provider: University of Pennsylvania
+Course Link: https://www.coursera.org/learn/decision-making-scenarios
+
+5. Strategic Thinking Skills
+Provider: Macquarie University
+Course Link: https://www.coursera.org/learn/strategic-thinking-skills
+
+6. Analytical Thinking for Professionals
+Provider: IBM
+Course Link: https://www.coursera.org/learn/analytical-thinking-professionals
+
+7. Computational Thinking for Problem Solving
+Provider: University of Pennsylvania
+Course Link: https://www.coursera.org/learn/computational-thinking-problem-solving
